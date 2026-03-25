e712d3d08cbc5d60595a5fff9cb4d0e7080a7845
Vini Lazev <vinicius.azevedo@sistemaseinformacao.com.br>
2026-03-24 22:51:08 -0300

    Remoção da validação durante a instalação do composer que quebraria a cada atualização

--- utils/installer.php
+++ utils/installer.php
@@ -89,12 +89,6 @@ while($simnao != 's' && $simnao != 'n');
 
 if(strtolower($simnao) == 's') {
 	copy('https://getcomposer.org/installer', 'composer-setup.php');
-	if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') {
-		echo 'Composer Installer verified';
-	} else {
-		echo 'Composer Installer corrupt';
-		unlink('composer-setup.php');
-	}
 	echoc();
 
 	shell_exec('php composer-setup.php');
